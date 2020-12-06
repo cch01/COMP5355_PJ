9b3f5bc496aa69193635cb324ca6280109eef6fc
cch01 <chanchiho01@gmail.com>
2020-12-06 22:04:32 +0800

    remove webpack, updated model, updated checklist

--- index.ts
+++ index.ts
@@ -1,52 +1,49 @@
-import {launch} from "puppeteer";
+import { launch } from "puppeteer";
 import 'console.table';
 import { Cookie } from "./models";
-import HTML from "./html"
+import HTML from "./html";
+import { checklist } from "./checklist";
 
-const URLs: string[] = [
-    "https://www.ons.gov.uk/visualisations/dvc368/dashboard/index.html",
-    "https://www.ons.gov.uk/visualisations/dvc638/maps/flow/",
+const getCookiesFromPage = async () => {
+  return new Promise(async (resolve, reject) => {
+    const cookieGets = checklist.map((url, index) => {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const browser = await launch();
+          const page = await browser.newPage();
+          await page.setDefaultTimeout(90000);
+          console.log(`navigating to ${url} ...`)
+          await page.goto(url).catch(e => null);
+          await page.screenshot({path: index+'.png'});
+          const data = await (page as any)._client.send('Network.getAllCookies');
+          await browser.close();
+          const cookiesWithUrl = data.cookies.map(cookie => { return { originalWebsite: url, ...cookie  } });
+          resolve(cookiesWithUrl);
+        } catch (error) {
+          console.error(error)
 
-]
-
-const getCookiesFromPage = async() => {
-    return new Promise(async(resolve, reject) => {
-        const cookieGets = URLs.map((url, index) => {
-            return new Promise(async(resolve, reject) => {
-                try {
-                    const browser = await launch();
-                    const page = await browser.newPage();
-                    console.log(`navigating to ${url} ...`)
-                    await page.goto(url);
-                    //await page.screenshot({path: index+'.png'});
-                    const data = await (page as any)._client.send('Network.getAllCookies'); 
-                    await browser.close();
-                    resolve(data.cookies)
-                }catch(error) {
-                    console.error(error)
-                    reject(error)
-                }
-            })
-        })
-        await Promise.all(cookieGets).then(result => {resolve(result)}).catch((error)=>{reject(error)})     
+        }
+      })
     })
+    await Promise.all(cookieGets).then(result => { resolve(result) }).catch((error) => { })
+  })
 }
 
 const arrFlatten = (arr: any): Array<Cookie> => {
-    return [].concat.apply([], arr)
+  return [].concat.apply([], arr)
 }
 
 // const removeDuplicates = (arr) => {
 //     return [...new Set(arr)]
 // }
 
-(async ()=> {
-    const allCookies = await getCookiesFromPage().then(result => {
-        return arrFlatten(result)
-    })
-    //const uniqueCookies = removeDuplicates(allCookies)
-    console.clear()
-    console.table(allCookies)
-    console.log(`Total of ${allCookies.length} cookies found`)
-    HTML.createFile(allCookies)
+(async () => {
+  const allCookies = await getCookiesFromPage().then(result => {
+    return arrFlatten(result)
+  })
+  //const uniqueCookies = removeDuplicates(allCookies)
+  // console.clear()
+  // console.table(allCookies)
+  console.log(`Total of ${allCookies.length} cookies found`)
+  HTML.createFile(allCookies)
 })();
--- models.ts
+++ models.ts
@@ -1,12 +1,12 @@
-export interface Cookie {
-    name: string,                
-    value: string,                                                                  
-    domain: string,       
-    path: string,  
-    expires: number,            
-    size: number,  
-    httpOnly: boolean,  
-    secure: boolean,  
-    session: boolean,  
-    sameSite?: string,
+export interface Cookie extends Object {
+  name: string,
+  value: string,
+  domain: string,
+  path: string,
+  expires: number,
+  size: number,
+  httpOnly: boolean,
+  secure: boolean,
+  session: boolean,
+  sameSite: string,
 }
